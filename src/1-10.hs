cd8ccb517cc9f8c0515a8148b61b87d8bcb309d9
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-12-12 18:50:22 +0800

    write 7. myFlatten)

--- src/1-10.hs
+++ src/1-10.hs
@@ -18,3 +18,22 @@ elementAt _ 0 = Nothing
 elementAt [] _ = Nothing
 elementAt (x:_) 1 = Just x
 elementAt (_:xs) n = elementAt xs (n - 1)
+
+myLength :: [a] -> Int
+myLength = foldr (const (+1)) 0
+
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = xs == ys
+  where ys = myReverse xs
+
+-- myFlatten --
+data NestedList a = Elem a | List [NestedList a]
+
+myFlatten :: NestedList a -> [a]
+myFlatten (Elem x) = [x]
+myFlatten (List []) = []
+myFlatten (List xs) = foldr ((++) . myFlatten) [] xs
+---------------
