6a748cd4f99c43c16b7f3f5f771d2d1c86052f85
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-12-12 18:52:21 +0800

    edit 7. myFlatten)

--- src/1-10.hs
+++ src/1-10.hs
@@ -34,6 +34,5 @@ data NestedList a = Elem a | List [NestedList a]
 
 myFlatten :: NestedList a -> [a]
 myFlatten (Elem x) = [x]
-myFlatten (List []) = []
 myFlatten (List xs) = foldr ((++) . myFlatten) [] xs
 ---------------
