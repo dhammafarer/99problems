137e152eb9c53c226f1f35ef33ec7e014cff7dfa
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-12-13 18:08:51 +0800

    write problem 13

--- src/11-20.hs
+++ src/11-20.hs
@@ -12,3 +12,25 @@ encodeMod = map f . L.group
         f xs  = Multiple (length xs) (head xs)
 
 -------------------------------------------------------------------------------
+
+decodeMod :: [Multilist a] -> [a]
+decodeMod = concatMap f
+  where
+    f (Multiple n c) = replicate n c
+    f (Single c)     = [c]
+
+-------------------------------------------------------------------------------
+
+encodeDirect :: Eq a => [a] -> [Multilist a]
+encodeDirect = foldr f []
+  where
+    f x [] = [Single x]
+    f x x'@(Single y : ys)
+      | x == y    = Multiple 2 x : ys
+      | otherwise = Single x : x'
+    f x x'@(Multiple n y : ys)
+      | x == y    = Multiple (n+1) x : ys
+      | otherwise = Single x : x'
+
+-------------------------------------------------------------------------------
+
